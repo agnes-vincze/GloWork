2e2caae3810a25f7b80ceb57827c34e859f125aa
Agnes Vincze <agnes-vincze@hotmail.com>
2025-07-01 17:56:33 +0100

    fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,18 +4,17 @@ class QuestionsController < ApplicationController
     @question = Question.new # for form
   end
 
-  def create
-    @questions = current_user.questions # needed in case of validation error
-    @question = Question.new(question_params)
-    @question.user = current_user
-    if @question.save
-      ChatbotJob.perform_later(@question)
-      respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.append(:questions, partial: "questions/question",
-            locals: { question: @question })
-        end
-        format.html { redirect_to questions_path }
+def create
+  @questions = current_user.questions # needed in case of validation error
+  @question = Question.new(question_params)
+  @question.user = current_user
+
+  if @question.save
+    ChatbotJob.perform_later(@question)
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.append(:questions, partial: "questions/question",
+          locals: { question: @question })
       end
       format.html { redirect_to questions_path }
     end
@@ -24,9 +23,7 @@ class QuestionsController < ApplicationController
   end
 end
 
-
-
-  private
+private
 
   def question_params
     params.require(:question).permit(:user_question)
